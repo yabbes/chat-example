1f49cac5322981e364cd2b1ba19b8c4fce6c7f6d
Tobias Machulski <yabbaspost@gmail.com>
2017-07-27 01:53:21 +0200

    yo

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var express = require('express');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views/'));
 
 app.get('/', function(req, res){
   res.render('pages/index', {
